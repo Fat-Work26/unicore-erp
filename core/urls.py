84a32356151f173d22426fdc120095b6a4fb873c
Fat Work <work.fat26@gmail.com>
2026-07-31 10:21:30 +0000

    refactor(templates):setup modular folder structure for public and app layoutes

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='home.html'),name='home'),
+    path('',TemplateView.as_view(template_name='public/home.html'),name='home'),
     path('accounts/',include('accounts.urls')),
     path('teachers/',include('teachers.urls')),
 
